2904d3ef5542c3774c5bcbef2831e048b26a3447
Daniils Petrovs <daniils@streamelements.com>
2025-09-17 12:39:45 +0200

    Add work page

---
+++ src/components/CompanyLink.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { LinkPreview } from 'bits-ui';
+
+  let className: string | undefined = undefined;
+  export let href: string;
+  export let contentClass: string | undefined = undefined;
+  export { className as class };
+</script>
+
+<LinkPreview.Root>
+  <LinkPreview.Trigger {href} class={className}>
+    <slot />
+  </LinkPreview.Trigger>
+  <LinkPreview.Content class={contentClass}
+    >
+    <slot name="preview" />
+  </LinkPreview.Content>
+</LinkPreview.Root>
+
+
